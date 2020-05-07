35661b1c2a749801dfc0bfd106fb50a2fb72cb0c
Anna_01 <396833147@qq.com>
2020-05-07 11:51:41 +0800

    我他妈提交了个信息嗷

---
+++ src/main/java/com/wanna0816/gitstudy1/GitStudy1Application.java
@@ -0,0 +1,13 @@
+package com.wanna0816.gitstudy1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitStudy1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitStudy1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/wanna0816/gitstudy1/GitStudy1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanna0816.gitstudy1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitStudy1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
